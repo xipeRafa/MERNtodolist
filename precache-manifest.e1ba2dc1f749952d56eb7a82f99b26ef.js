eddfacf66fe486e9a37293db64e48f27608c0f71
rafael <rafaelsistem87@gmail.com>
2022-01-15 01:22:37 -0700

    Updates

---
+++ precache-manifest.e1ba2dc1f749952d56eb7a82f99b26ef.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce2d543db3dafc4e18882ef4cc6eccda",
+    "url": "/MERNtodolist/index.html"
+  },
+  {
+    "revision": "a5c78f72da36a87992ca",
+    "url": "/MERNtodolist/static/js/2.7620b114.chunk.js"
+  },
+  {
+    "revision": "599afc0ac91145d7946a",
+    "url": "/MERNtodolist/static/js/main.d7daf0b2.chunk.js"
+  },
+  {
+    "revision": "5eb5ff09ce5d2a6eb682",
+    "url": "/MERNtodolist/static/js/runtime~main.3ea54116.js"
+  }
+]);
